900554ad8e4803f22f3569ae1a2f499bc3ed58fa
Xiaolong Wang <xlwang@fb.com>
2017-03-28 00:50:47 -0700

    dev change

--- run.sh
+++ run.sh
@@ -2,26 +2,30 @@
 
 make plans/train NOEXEC=1;
 
+thread=46
 iter=20
-
-gd=1e-2
-./bin/plans/train \
-  V_MODEL_DECOR_FILE_PATH iter${iter}_gd${gd} \
-  V_ITER_NUM ${iter} \
-  V_THREAD_NUM 46 \
-  V_INIT_GRAD_DESCENT_STEP_SIZE ${gd}
+temp=5
 
 gd=5e-3
 ./bin/plans/train \
-  V_MODEL_DECOR_FILE_PATH iter${iter}_gd${gd} \
+  V_MODEL_DECOR_FILE_PATH iter${iter}_gd${gd}_temp${temp} \
   V_ITER_NUM ${iter} \
-  V_THREAD_NUM 46 \
-  V_INIT_GRAD_DESCENT_STEP_SIZE ${gd}
+  V_THREAD_NUM ${thread} \
+  V_INIT_GRAD_DESCENT_STEP_SIZE ${gd} \
+  V_FINAL_INV_TEMP ${temp}
+
+gd=2e-3
+./bin/plans/train \
+  V_MODEL_DECOR_FILE_PATH iter${iter}_gd${gd}_temp${temp} \
+  V_ITER_NUM ${iter} \
+  V_THREAD_NUM ${thread} \
+  V_INIT_GRAD_DESCENT_STEP_SIZE ${gd} \
+  V_FINAL_INV_TEMP ${temp}
 
 gd=1e-3
 ./bin/plans/train \
-  V_MODEL_DECOR_FILE_PATH iter${iter}_gd${gd} \
+  V_MODEL_DECOR_FILE_PATH iter${iter}_gd${gd}_temp${temp} \
   V_ITER_NUM ${iter} \
-  V_THREAD_NUM 46 \
-  V_INIT_GRAD_DESCENT_STEP_SIZE ${gd}
-
+  V_THREAD_NUM ${thread} \
+  V_INIT_GRAD_DESCENT_STEP_SIZE ${gd} \
+  V_FINAL_INV_TEMP ${temp}
